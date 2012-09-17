1013597fbbcca50c9ebda7f54ffd1df0294ff80f
Gabriel Mansour <gabrielmansour@gmail.com>
2012-09-17 00:19:57 -0400

    resolve merge conflict

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashpool::Application.config.secret_token = '6d4c76c9a236f751f00795c1f151f576fdb70997c9b8d52eee2e89f8b9394214685b4008deed40b6362f876fbda356b5d7e84897e769ceaad7204b26f13b139c'
